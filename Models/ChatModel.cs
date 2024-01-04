425f5ff93e8c7e2cf2592beff381a1d9b1885af6
matheusjean28 <matheusjean28@gmail.com>
2024-01-04 18:44:07 -0300

    start to create user chatroom list

---
+++ Models/ChatModel.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using UserModel;
+
+namespace ChatModel
+{
+    public class Chat
+    {
+        [Key]
+        public string ChatID { get; set; }
+
+        [Required(ErrorMessage = "ChatName is required")]
+        public string ChatName { get; set; }
+
+        //if not declared, defoult value is "10"
+        public int OnlineUser { get; set; } = 10;
+
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
