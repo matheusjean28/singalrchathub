fdc225e895546f901ba81aeceefcd064efcdac4f
matheusjean28 <matheusjean28@gmail.com>
2024-02-23 18:51:20 -0300

    using bcrypt to hash passwords at database

--- Controller/AuthController.cs
+++ Controller/AuthController.cs
@@ -1,15 +1,14 @@
-using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Threading.Tasks;
+using AuthServiceJwt;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Logging;
-
 using UserContext;
-using UserModel;
 using UserLoginDTO;
 using UserLoginModel;
-using AuthServiceJwt;
+using UserModel;
 
 namespace AuthControllerMethod
 {
@@ -19,51 +18,55 @@ namespace AuthControllerMethod
         private readonly UserDbContext _context;
         private readonly AuthService _authJwt;
 
-        public AuthController(UserDbContext context, AuthService authJwt, ILogger<UserModel.User> logger)
+        public AuthController(
+            UserDbContext context,
+            AuthService authJwt,
+            ILogger<UserModel.User> logger
+        )
         {
             _context = context;
-            _authJwt  = authJwt;
+            _authJwt = authJwt;
             _logger = logger;
         }
 
         [Route("/CreateUser")]
         [HttpPost]
-        public async Task<ActionResult<User>> CreateUser([FromBody]UserModel.User user)
-        {   
-        try
+        public async Task<ActionResult<User>> CreateUser([FromBody] UserModel.User user)
         {
-        if (user == null)
-        {
-            return BadRequest("Unexpected error occurred");
-        }
+            try
+            {
+                if (user == null)
+                {
+                    return BadRequest("Unexpected error occurred");
+                }
 
-        if (await CheckIfUserExist(user))
-        {
-            var userAlreadyExistMessage = $"{user.UserName} already exists";
-            return Conflict(userAlreadyExistMessage);
-        }
+                if (await CheckIfUserExist(user))
+                {
+                    var userAlreadyExistMessage = $"{user.UserName} already exists";
+                    return Conflict(userAlreadyExistMessage);
+                }
 
-        var newUser = new User(user.UserName, user.Email, user.Pass, user.Gener);
+                string hashedPassword = BCrypt.Net.BCrypt.HashPassword(user.Pass);
+                var newUser = new User(user.UserName, user.Email, hashedPassword, user.Gener);
+                _context.Users.Add(newUser);
+                await _context.SaveChangesAsync();
+                var authToken = await _authJwt.GenerateJwtToken(user.UserName);
 
-        _context.Users.Add(newUser);
-        await _context.SaveChangesAsync();
-        var authToken = await _authJwt.GenerateJwtToken(user.UserName);
+                var response = new
+                {
+                    id = newUser.Id,
+                    userName = newUser.UserName,
+                    email = newUser.Email,
+                    token = new { token = authToken }
+                };
 
-        var response = new
-        {
-            id = newUser.Id,
-            userName = newUser.UserName,
-            email = newUser.Email,
-            token = new { token = authToken }
-        };
-
-        return Ok(response);
-        }
-        catch (Exception ex)
-        {
-        Console.WriteLine(ex);
-        return BadRequest("An error occurred during account creation."); 
-        }
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return BadRequest("An error occurred during account creation.");
+            }
         }
 
         [Authorize]
@@ -74,73 +77,75 @@ namespace AuthControllerMethod
             return await _context.Users.ToListAsync();
         }
 
-
-        //check other params also, 
+        //check other params also,
         //return DTO that contains name, userId, gender only
         [Route("Auth")]
         [HttpPost]
-        public async Task<IActionResult> Auth([FromBody]UserLogin user)
+        public async Task<IActionResult> Auth([FromBody] UserLogin user)
         {
             try
             {
-            var _searchedUser = await _context.Users.FirstOrDefaultAsync(u => u.UserName == user.UserName);
+                var searchedUser = await _context.Users.FirstOrDefaultAsync(u =>
+                    u.UserName == user.UserName
+                );
 
-            if (_searchedUser  != null)
-            {
-                if( IsValidPassword(_searchedUser.Pass, user.Password))
+                if (searchedUser != null)
                 {
+                    if (isValidPasswordComparedWithHashed(searchedUser.Pass, user.Password))
+                    {
+                        var userToken = new
+                        {
+                            token = await _authJwt.GenerateJwtToken(user.UserName)
+                        };
 
-                var _userToken = new { token = await _authJwt.GenerateJwtToken(user.UserName) };
-               
-                var _responseAuthUserOk = new UserDTO
-                {   
-                    Id = _searchedUser.Id,
-                    UserName= _searchedUser.UserName,
-                    Email= _searchedUser.Email,
-                    Token= _userToken
-                };
-                
-                return Ok(_responseAuthUserOk);
+                        var responseAuthUserOk = new UserDTO
+                        {
+                            Id = searchedUser.Id,
+                            UserName = searchedUser.UserName,
+                            Email = searchedUser.Email,
+                            Token = userToken
+                        };
+
+                        return Ok(responseAuthUserOk);
+                    }
+                    else
+                    {
+                        return BadRequest("Wrong Password!");
+                    }
                 }
-                else {
-                    return BadRequest("Wrong Password!");
-                }
-            };
 
-            var _notFound = $"{user.UserName} Was not Found.";
-            return NotFound(_notFound);
-
+                var notFoundMessage = $"{user.UserName} was not found.";
+                return NotFound(notFoundMessage);
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
                 Console.WriteLine($"Error: {ex}");
-                Console.WriteLine("Error:", ex.Message);
-                return BadRequest("An error occurred during authentication."); 
+                return BadRequest("An error occurred during authentication.");
             }
         }
 
-
-        //move to other component after finish  
+        //move to other component after finish
         public async Task<bool> CheckIfUserExist(User user)
         {
-            var userNameExist = await _context.Users.AnyAsync(u => 
-            u.UserName == user.UserName );
-            var userEmailExist = await _context.Users.AnyAsync(u => 
-            u.Email == user.Email );
-            
-            if(userNameExist && userEmailExist){
+            var userNameExist = await _context.Users.AnyAsync(u => u.UserName == user.UserName);
+            var userEmailExist = await _context.Users.AnyAsync(u => u.Email == user.Email);
+
+            if (userNameExist && userEmailExist)
+            {
                 return true;
             }
             return false;
-
         }
 
-        public bool IsValidPassword(string paswordFromParams,  string passwordFromDb)
+        public bool IsValidPassword(string paswordFromParams, string passwordFromDb)
         {
             var isEqualPass = passwordFromDb.Trim().Equals(paswordFromParams.Trim());
             return isEqualPass;
         }
+
+        public bool isValidPasswordComparedWithHashed(string hashedPassword, string plainPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(plainPassword, hashedPassword);
+        }
     }
 }
-
-    
